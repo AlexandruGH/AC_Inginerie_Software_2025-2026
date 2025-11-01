0d2aaa02887cdd8483d1010ee37a6407c68e4f0d
AlexandruGH <alexandru.ghiurutan@yahoo.com>
2025-11-01 13:27:06 +0200

    First Library project iteration with sql injection attack and defense example

--- OurLibrary/src/main/java/Main.java
+++ OurLibrary/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import database.DatabaseConnectionFactory;
+import database.JDBConnectionWrapper;
 import model.Book;
 import model.builder.BookBuilder;
+import repository.BookRepository;
+import repository.BookRepositoryMock;
+import repository.BookRepositoryMySQL;
+import service.BookService;
+import service.BookServiceImpl;
+
+import java.sql.Connection;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
@@ -14,10 +24,19 @@ public class Main {
         Book framBook = new BookBuilder()
                 .setTitle("Fram Ursul Polar")
                 .setId(1L)
-                .setAuthor("Cezar Petrescu")
+                .setAuthor("', '', null); DROP TABLE book; -- ")
+                .setPublishedDate(LocalDate.of(1940, 01, 01))
                 .build();
 
         System.out.println(framBook);
+
+        JDBConnectionWrapper jdbConnectionWrapper = DatabaseConnectionFactory.getConnectionWrapper(false);
+        Connection connection = jdbConnectionWrapper.getConnection();
+
+        BookRepository bookRepository = new BookRepositoryMySQL(connection);
+        BookService bookService = new BookServiceImpl(bookRepository);
+        bookService.save(framBook);
+        System.out.println(bookService.findAll());
 
 
     }
